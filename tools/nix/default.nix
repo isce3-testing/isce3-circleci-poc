b7bdf0ef979989d0384763f0130e5b775ff9afa7
Ryan Burns <rtburns@jpl.nasa.gov>
2021-12-15 13:50:33 -0800

    Give the cache a catchy name

--- tools/nix/default.nix
+++ tools/nix/default.nix
@@ -1,4 +1,5 @@
 { pkgs ? import (fetchTarball {
+    # Remember to update the cache key in .circleci/config.yml when bumping!
     url = "https://github.com/NixOS/nixpkgs/archive/5f5bc79a1bf33493a5fb0e599c4f206fc0cb491e.tar.gz";
     sha256 = "1371nxfy4l2r190yf3ayzg1fcrasrn9laff0khmgwfsxrcam9a9h";
   }) {}
